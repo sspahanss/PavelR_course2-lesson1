87401d4c02817cffb86d2a29ce83c464942073ce
Parcifal <scar1987@gmail.com>
2020-08-17 00:46:29 +0800

    Initial Commit

---
+++ PavelR_course2-lesson1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PavelR_course2-lesson1
+//
+//  Created by Павел on 17.08.2020.
+//  Copyright © 2020 Павел. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
